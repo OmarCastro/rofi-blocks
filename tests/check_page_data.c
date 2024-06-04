1708f2cb50e593e06cc22eedeecbb4b41584668d
Omar Castro <omar.castro.360@gmail.com>
2024-06-04 17:25:18 -0400

    test: update check_page_data test

--- tests/check_page_data.c
+++ tests/check_page_data.c
@@ -15,7 +15,7 @@ int main(void)
     test_uint_equals(.result = page_data_get_number_of_lines(page_data), .expected = 0);
 
 
-    page_data_add_line(page_data, "aaa", "any-icon", "any-data", true, true, true);
+    page_data_add_line(page_data, "aaa", "any-icon", "any-data", true, true, true, true);
     test_string_equals(.result =  page_data_get_line_by_index_or_else(page_data, 0, NULL)->text, .expected= "aaa");
     test_true(page_data_get_line_by_index_or_else(page_data, -1, NULL) == NULL);
     test_true(page_data_get_line_by_index_or_else(NULL, 0, NULL) == NULL);
