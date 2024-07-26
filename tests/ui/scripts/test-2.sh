ec67466d9e0f6f5a72860321ca7b9c1c042a1c62
OmarCastro <omar.castro.360@gmail.com>
2024-07-26 20:44:17 +0100

    test: update test 2

--- tests/ui/scripts/test-2.sh
+++ tests/ui/scripts/test-2.sh
@@ -5,9 +5,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 echo '{ "lines": [ "one","two","three" ], "active entry": 1}'
 
 sleep 0.1
-echo '{ "lines": [ "one","two","three" ], "active entry": 1}'
-sleep 0.1
-
 import -window root "../assets/tests/test-2/result.png" >&2
 sleep 0.2
 
