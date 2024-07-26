b4f10fe343134104f139f3dcfaf4252df8d14819
OmarCastro <omar.castro.360@gmail.com>
2024-07-26 20:03:14 +0100

    build: allow installation for UI testing

--- dev-tools/github-action-build.sh
+++ dev-tools/github-action-build.sh
@@ -2,13 +2,33 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
 
+echo "==============="
+echo "=== COMPILE ==="
+echo "==============="
+
 meson setup build-test -Db_coverage=true --warnlevel 2 || exit 1
 meson compile -C build-test || exit 1
-meson install -C build-test
+
+echo "==============="
+echo "=== INSTALL ==="
+echo "==============="
+
+
+sudo meson install -C build-test
+
+echo "================"
+echo "===== TEST ====="
+echo "================"
+
 meson test --wrap='valgrind --leak-check=full' -C build-test
 ninja coverage -C build-test
 ls build-test/meson-logs/testlog.json || cp build-test/meson-logs/testlog-valgrind.json build-test/meson-logs/testlog.json 
 ls build-test/meson-logs/testlog.txt || cp build-test/meson-logs/testlog-valgrind.txt build-test/meson-logs/testlog.txt 
+
+echo "================"
+echo "=== DOCUMENT ==="
+echo "================"
+
 dev-tools/run docs
 rm -rf build-docs/reports/ui-images
 cp -r tests/ui/assets/tests build-docs/reports/ui-images
