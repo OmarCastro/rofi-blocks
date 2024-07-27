f10aac8358081705597dd1c81e7217822dd82aff
OmarCastro <omar.castro.360@gmail.com>
2024-07-27 20:59:31 +0100

    test: update scripts

--- tests/ui/scripts/test-2.sh
+++ tests/ui/scripts/test-2.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
 echo '{ "lines": [ "one","two","three" ], "active entry": 1}'
 
+# END TEST SCRIPT
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
 sleep 0.1
 import -window root "../assets/tests/test-2/result.png" >&2
-sleep 0.2
+sleep 0.1
 
