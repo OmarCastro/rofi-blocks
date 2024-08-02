26c491802cc0a74c6062247f365d30247948a9cb
OmarCastro <omar.castro.360@gmail.com>
2024-08-02 14:44:03 +0100

    test: fix hidden result image in slider

--- docs/test-report.js
+++ docs/test-report.js
@@ -1,44 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
+	const intersectionObserver = new IntersectionObserver((entries) => {
+		for(const entry of entries){
+			updateSliderDimensions(entry.target)
+		}
+	})
+
+	const resizeObserver = new ResizeObserver((entries) => {
+		for(const entry of entries){
+			updateSliderDimensions(entry.target)
+		}
+	})
+
+
 	const sliders = document.querySelectorAll(".comparison-slider")
 	for (const slider of sliders) {
-		const imgWidth = slider.querySelector(':scope > img').getBoundingClientRect().width + "px";
-		const divider = slider.querySelector('.divider')
+		updateSliderDimensions(slider)
+		addSliderDragBehaviour(slider);
+		intersectionObserver.observe(slider)
+		resizeObserver.observe(slider)
+	}
+
+	function updateSliderDimensions(slider){
+		const imgWidth = slider.querySelector(':scope > img').getBoundingClientRect().width + "px"
 		const resize = slider.querySelector('.resize')
 		resize.querySelector("img").style.width = imgWidth
-		drags(divider, resize, slider);
-		
-
-		window.addEventListener("resize", function() {
-			const imgWidth = slider.querySelector(':scope > img').getBoundingClientRect().width + "px";
-			slider.querySelector(".resize img").style.width = imgWidth
-		});
 	}
-});
 
+	/**
+	* @param {HTMLElement} slider
+	*/
+	function addSliderDragBehaviour(slider) {
+		const dragElement = slider.querySelector('.divider')
+		const resizeElement = slider.querySelector('.resize')
 
-/**
-* @param {HTMLElement} dragElement
-* @param {HTMLElement} resizeElement
-* @param {HTMLElement} container
-*/
-function drags(dragElement, resizeElement, container) {
+		let dragWidth, containerOffset, containerWidth, minLeft, maxLeft
 
-// This creates a variable that detects if the user is using touch input insted of the mouse.
-let touched = false;
-window.addEventListener('touchstart', function() { touched = true; });
-window.addEventListener('touchend', function() { touched = false; });
+		const pointermovehandler = function(event) {
 
-
-let dragWidth, containerOffset, containerWidth, minLeft, maxLeft
-
-const pointermovehandler = function(e) {
-			
-			// if the user is not using touch input let do preventDefault to prevent the user from slecting the images as he moves the silder arround.
-			if ( touched === false ) {
-				e.preventDefault();
+			// if the user is using mouse, use preventDefault to prevent the user from
+			// selecting the images as he moves the silder arround.
+			if ( event.pointerType === "mouse" ) {
+				event.preventDefault();
 			}
 			
-			let moveX = e.clientX
+			let moveX = event.clientX
 			let leftValue = moveX - dragWidth;
 
 			// stop the divider from going over the limits of the container
@@ -52,35 +57,27 @@ const pointermovehandler = function(e) {
 
 			document.querySelector(".dragging").style.left = widthValue
 			document.querySelector(".resizing").style.width = widthValue;
-			
 		}
 
-// clicp the image and move the slider on interaction with the mouse or the touch input
-dragElement.addEventListener("pointerdown", function(e) {
-		
-		//add classes to the emelents - good for css animations if you need it to
-		dragElement.classList.add("dragging");
-		resizeElement.classList.add("resizing");
-		//create vars
-		dragWidth = dragElement.getBoundingClientRect().width;
-		containerOffset = container.getBoundingClientRect().left;
-		containerWidth = container.getBoundingClientRect().width;
-		minLeft = containerOffset + 10;
-		maxLeft = containerOffset + containerWidth - dragWidth - 10;
-		
-		//add event listner on the divider emelent
-		dragElement.parentElement.addEventListener("pointermove", pointermovehandler)
-	
-	})
-	
-	
-	dragElement.addEventListener("pointerup", function(e) {
-		// stop clicping the image and move the slider
-		dragElement.classList.remove("dragging");
-		resizeElement.classList.remove("resizing");
-		dragElement.parentElement.removeEventListener("pointermove", pointermovehandler)
+		dragElement.addEventListener("pointerdown", () => {
+			
+			dragElement.classList.add("dragging");
+			resizeElement.classList.add("resizing");
 
-	
-	});
-	
-}
+			dragWidth = dragElement.getBoundingClientRect().width;
+			containerOffset = slider.getBoundingClientRect().left;
+			containerWidth = slider.getBoundingClientRect().width;
+			minLeft = containerOffset + 10;
+			maxLeft = containerOffset + containerWidth - dragWidth - 10;
+
+			window.addEventListener("pointermove", pointermovehandler)
+
+			window.addEventListener("pointerup", () => {
+				// stop clicping the image and move the slider
+				dragElement.classList.remove("dragging");
+				resizeElement.classList.remove("resizing");
+				window.removeEventListener("pointermove", pointermovehandler)
+			}, { once: true });
+		})
+	}
+});
