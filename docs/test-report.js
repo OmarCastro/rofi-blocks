237eed6fb531e30c93c22fe46ba3cb3d988bced0
OmarCastro <omar.castro.360@gmail.com>
2024-08-04 15:06:04 +0100

    test: expand all on print

--- docs/test-report.js
+++ docs/test-report.js
@@ -81,3 +81,19 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 	}
 });
+
+window.matchMedia("print").addEventListener("change", evt => {
+    if (evt.matches) {
+        elms = document.body.querySelectorAll("details:not([open])");
+        for (e of elms) {
+            e.setAttribute("open", "");
+            e.dataset.wasclosed = "";
+        }
+    } else {
+        elms = document.body.querySelectorAll("details[data-wasclosed]");
+        for (e of elms) {
+            e.removeAttribute("open");
+            delete e.dataset.wasclosed;
+        }
+    }
+})
